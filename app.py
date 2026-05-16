0f9c6d40b2e21de574cae287188756f32d53f74c
Abhinav Shivanath Kamoji <83529591+abhinav-kamoji@users.noreply.github.com>
2026-05-17 00:00:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
 from setuptools import setup, find_packages
+
+setup(
+    name="weather-cli",
+    version="1.0",
+    packages=find_packages(),  
+    install_requires=[
+        "typer",
+        "requests",
+        "yaspin",
+        "python-dotenv",
+        "inquirer",
+    ],
+
+      entry_points={
+        "console_scripts": [
+            "weather=weather_cli.main:app",
+            "weather-config=weather_cli.config:setup"
+        ],
+    },
+)
